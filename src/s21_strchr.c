0c7e376f27db11ac7642c613a41abd1abbb028b4
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-19 04:16:09 +0300

    many style changes and tests-related changes to makefile

--- src/s21_strchr.c
+++ src/s21_strchr.c
@@ -1,6 +1,6 @@
 #include "s21_string.h"
 
-char *s21_strchr(const char *str, int c) {
+char *s21_strchr(const char* str, int c) {
     if (!str) return S21_NULL;
     const char *ptr = str;
     while (*ptr!= '\0') {
