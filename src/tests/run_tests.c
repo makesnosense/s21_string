a3376ed4b0b43ed89f481c67dfe6285e160c61e6
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-19 20:52:29 +0300

    stated gcov but unsuccessfully, added stract suite

--- src/tests/run_tests.c
+++ src/tests/run_tests.c
@@ -5,19 +5,26 @@
 
  int main()
  {  
-    int number_failed;
-    Suite* sui;
-    SRunner* suite_runner;
+   int number_failed;
+   Suite* strlen_suite;
+   Suite* strcat_suite;
+
+   SRunner* suite_runner;
 
 
-    sui = make_strlen_suite();
-    suite_runner = srunner_create(sui);
+   strlen_suite = make_strlen_suite();
+   strcat_suite = make_strcat_suite();
 
-    srunner_run_all(suite_runner, CK_NORMAL);
-
-    number_failed = srunner_ntests_failed(suite_runner);
-    srunner_free(suite_runner);
+   suite_runner = srunner_create(strlen_suite);
+   srunner_add_suite(suite_runner, strcat_suite);
 
 
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+   srunner_run_all(suite_runner, CK_VERBOSE);
+
+   number_failed = srunner_ntests_failed(suite_runner);
+   srunner_free(suite_runner);
+
+
+   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
  }
