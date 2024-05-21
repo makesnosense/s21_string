a65cefa994ebc6511ec40851e776976342b2c123
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-21 19:45:16 +0300

    memchr fix + new tests

--- src/tests/run_tests.h
+++ src/tests/run_tests.h
@@ -1,12 +1,13 @@
 #ifndef _RUN_TESTS_H_
 #define _RUN_TESTS_H_
 
-#include "../s21_string.h"
-#include <stdlib.h>
 #include <check.h>
+#include <stdlib.h>
 
+#include "../s21_string.h"
 
 Suite* make_strlen_suite();
 Suite* make_strcat_suite();
+Suite* make_memchr_suite();
 
 #endif
