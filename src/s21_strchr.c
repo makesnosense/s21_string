233a2a64ebdf8a57ac455a73ab3b24a9298a7910
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-21 19:50:11 +0300

    style fixes

--- src/s21_strchr.c
+++ src/s21_strchr.c
@@ -1,14 +1,14 @@
 #include "s21_string.h"
 
 char* s21_strchr(const char* str, int c) {
-    if (!str) return S21_NULL;
-    const char* ptr = str;
-    while (*ptr!= '\0') {
-        if (*ptr == c) {
-            return (char*) ptr;
-        }
-        ptr++;
+  if (!str) return S21_NULL;
+  const char* ptr = str;
+  while (*ptr != '\0') {
+    if (*ptr == c) {
+      return (char*)ptr;
     }
+    ptr++;
+  }
 
-    return S21_NULL;
+  return S21_NULL;
 }
