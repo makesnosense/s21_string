384bba07a2b098507675fb378109dbb7159574ca
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-22 03:02:31 +0300

    memcmp + tests

--- src/s21_memcmp.c
+++ src/s21_memcmp.c
@@ -3,15 +3,12 @@
 int s21_memcmp(const void* str1, const void* str2, s21_size_t n) {
   const unsigned char* ptr1 = (const unsigned char*)str1;
   const unsigned char* ptr2 = (const unsigned char*)str2;
+  int result = 0;  //
 
-  if (n > sizeof(ptr1)) {
-    return -1;
-  }
-
-  for (s21_size_t i = 0; i < n; ++i) {
+  for (s21_size_t i = 0; i < n && result == 0; ++i) {
     if (ptr1[i] != ptr2[i]) {
-      return ptr1[i] - ptr2[i];
+      result = ptr1[i] - ptr2[i];
     }
   }
-  return 0;
+  return result;
 }
