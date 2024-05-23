6ad8049e7239194d55ca32f06bd58be8365432b8
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-23 06:27:54 +0300

    memmove tests done

--- src/tests/test_memmove.c
+++ src/tests/test_memmove.c
@@ -1,5 +1,6 @@
-#include "run_tests.h"
+#include <stdio.h>
 
+#include "run_tests.h"
 START_TEST(test_memmove_empty) {
   char src[1] = {0};
   char dest[1];
@@ -21,27 +22,30 @@ START_TEST(test_memmove_content) {
 END_TEST
 
 START_TEST(test_memmove_overlap_forward) {
-  char src[] = "Overlap example";
-  char* dest = src + 2;
+  char dest[33] = "1.2.3.4.5.6.7.8.9.10.11.12.13.14";
+  char* src = dest;
 
-  void* result = s21_memmove(dest, src, 10);
-  ck_assert_mem_eq(dest, "Overlap ex", 10);
-  ck_assert_ptr_eq(result, dest);
+  char* lib_expected_result = "1.2.31.2.3.4.5.6.7.8.9.10.11.12.";
+
+  void* s21_result = memmove(dest + 5, src, 27);
+
+  ck_assert_mem_eq(dest, lib_expected_result, strlen(lib_expected_result));
+  ck_assert_ptr_eq(s21_result - 5, dest);
 }
 END_TEST
 
-// START_TEST(test_memmove_overlap_backward) {
-//   char src[] = "Another overlap example";
-//   char* dest = src;
+START_TEST(test_memmove_overlap_backward) {
+  char dest[] = "123456789.10.11.12.13.14";
+  char* src = dest;
 
-//   void* result = s21_memmove(dest, src + 5, 10);
-//   ck_assert_mem_eq(dest, "overlap exa", 10);
-//   ck_assert_mem_eq(
-//       dest, "overlap exaple",
-//       strlen("overlap exaple"));
-//   ck_assert_ptr_eq(result, dest);
-// }
-// END_TEST
+  char* lib_expected_result = "6789.10.11.12.13.14";
+
+  void* s21_result = s21_memmove(dest, src + 5, 25);
+
+  ck_assert_mem_eq(dest, lib_expected_result, strlen(dest));
+  ck_assert_ptr_eq(s21_result, dest);
+}
+END_TEST
 
 Suite* make_memmove_suite() {
   Suite* memmove_suite = suite_create("memmove");
@@ -51,7 +55,7 @@ Suite* make_memmove_suite() {
   tcase_add_test(tc_core, test_memmove_empty);
   tcase_add_test(tc_core, test_memmove_content);
   tcase_add_test(tc_core, test_memmove_overlap_forward);
-  // tcase_add_test(tc_core, test_memmove_overlap_backward);
+  tcase_add_test(tc_core, test_memmove_overlap_backward);
 
   suite_add_tcase(memmove_suite, tc_core);
   return memmove_suite;
