94bfc6058028ef3c82f04181352ceebcec2a5c52
mintasal <mintasal@student.21-school.ru>
2024-05-24 08:42:50 +0300

    meneg

--- src/s21_strrchr.c
+++ src/s21_strrchr.c
@@ -1,13 +1,15 @@
 #include "s21_string.h"
 
 char* s21_strrchr(const char* str, int c) {
+  int flag = 0;
   const char* p = str + s21_strlen(str) - 1;
   while (p >= str) {
     if (*p == c) {
-      return (char*)p;
+      flag++;
+      break;
     }
     p--;
   }
 
-  return S21_NULL;
+  return (flag == 0) ? S21_NULL : (char*)p;
 }
--- src/s21_strspn.c
+++ src/s21_strspn.c
@@ -1,23 +1,21 @@
 #include "s21_string.h"
 
-s21_size_t s21_strspn(const char* str1, const char* str2) {
-  if (!*str1 || !*str2) return 0;
-
-  s21_size_t count = 0;
-  while (*str1 != '\0') {
+s21_size_t s21_strspn(const char *str1, const char *str2) {
+  s21_size_t len1 = s21_strlen(str1);
+  s21_size_t len2 = s21_strlen(str2);
+  s21_size_t result = 0;
+  for (s21_size_t i = 0; i < len1; i++) {
     int found = 0;
-    while (*str2 != '\0' && *str1 != *str2) {
-      str2++;
+    for (s21_size_t j = 0; j < len2; j++) {
+      if (str1[i] == str2[j]) {
+        found = 1;
+        break;
+      }
     }
-    if (*str2 == *str1) {
-      found = 1;
+    if (!found) {
       break;
     }
-    if (found) {
-      str1++;
-      continue;
-    }
-    break;
+    result++;
   }
-  return count;
+  return result;
 }
