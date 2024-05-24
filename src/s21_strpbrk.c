743dea5e2521d3c329a19375f8613f6260c3184b
mintasal <mintasal@student.21-school.ru>
2024-05-24 08:42:50 +0300

    meneg

--- src/s21_strpbrk.c
+++ src/s21_strpbrk.c
@@ -1,13 +1,18 @@
 #include "s21_string.h"
 
 char* s21_strpbrk(const char* str1, const char* str2) {
+  int flag = 0;
   while (*str1 != '\0') {
     for (const char* ptr = str2; *ptr != '\0'; ptr++) {
       if (*str1 == *ptr) {
-        return (char*)str1;
+        flag++;
+        break;
       }
+    }
+    if(flag == 1){
+      break;
     }
     str1++;
   }
-  return S21_NULL;
+  return (flag == 0) ? S21_NULL : (char*)str1;
 }
