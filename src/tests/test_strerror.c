e9808952810f8906d440a7e4bc36dfcbe59f0543
mintasal <mintasal@student.21-school.ru>
2024-05-25 20:25:38 +0300

    strerror test fix

--- src/tests/test_strerror.c
+++ src/tests/test_strerror.c
@@ -13,23 +13,34 @@ START_TEST(test_strerror) {
 END_TEST
 
 START_TEST(test_strerror_unknown) {
-  int err_code = _i;  // supplied through add_loop_test func
+  int err_code = -3;
   if (!IS_EXCLUDED(err_code)) {
     char* lib_result = strerror(err_code);
     char* s21_result = s21_strerror(err_code);
     ck_assert_str_eq(s21_result, lib_result);
-    // free(lib_result);
+    free(lib_result);
   }
 }
 END_TEST
 
-// Функция для добавления всех тестов
+START_TEST(test_strerror_unknown_above) {
+  int err_code = 333;  
+  if (!IS_EXCLUDED(err_code)) {
+    char* lib_result = strerror(err_code);
+    char* s21_result = s21_strerror(err_code);
+    ck_assert_str_eq(s21_result, lib_result);
+    free(lib_result);
+  }
+}
+END_TEST
+
 Suite* make_strerror_suite() {
   Suite* suite = suite_create("strerror");
   TCase* tc = tcase_create("core");
 
   tcase_add_loop_test(tc, test_strerror, 0, ERR_COUNT);
-  tcase_add_loop_test(tc, test_strerror_unknown, -10, -1);
+  tcase_add_test(tc, test_strerror_unknown);
+  tcase_add_test(tc, test_strerror_unknown_above);
 
   suite_add_tcase(suite, tc);
   return suite;
