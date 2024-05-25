d426cb1cc41548a9a11f2f2003cdb1cb68da0930
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-25 12:42:48 +0300

    strspn done

--- src/s21_strspn.c
+++ src/s21_strspn.c
@@ -4,18 +4,18 @@ s21_size_t s21_strspn(const char *str1, const char *str2) {
   s21_size_t len1 = s21_strlen(str1);
   s21_size_t len2 = s21_strlen(str2);
   s21_size_t result = 0;
-  for (s21_size_t i = 0; i < len1; i++) {
-    int found = 0;
-    for (s21_size_t j = 0; j < len2; j++) {
+  int found = 1;
+
+  for (s21_size_t i = 0; i < len1 && found; i++) {
+    found = 0;
+    for (s21_size_t j = 0; j < len2 && !found; j++) {
       if (str1[i] == str2[j]) {
         found = 1;
-        break;
       }
     }
-    if (!found) {
-      break;
+    if (found) {
+      result++;
     }
-    result++;
   }
   return result;
 }
