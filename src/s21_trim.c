100de59a79b966a9ee6cddf1a6784467c8ccd1d9
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-27 08:13:55 +0300

    trim tests done + small fix (removed after calloc if))

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -1,25 +1,22 @@
-#include "s21_string.h"
-
 #include <stdlib.h>
 
-void *s21_trim(const char *src, const char *trim_chars) {
-  char *result = S21_NULL;
+#include "s21_string.h"
+
+void* s21_trim(const char* src, const char* trim_chars) {
+  char* result = S21_NULL;
   if (src) {
     if (!trim_chars) {
       trim_chars = " ";
     }
-    char *ptr_str = (char *)src;
-    char *end_str = (char *)src + s21_strlen(src);
-    for (; *ptr_str && s21_strchr(trim_chars, *ptr_str); ptr_str++)
-      ;
+    char* ptr_str = (char*)src;
+    char* end_str = (char*)src + s21_strlen(src);
+    for (; *ptr_str && s21_strchr(trim_chars, *ptr_str); ptr_str++);
     for (; end_str != ptr_str && s21_strchr(trim_chars, *(end_str - 1));
-         end_str--)
-      ;
-    result = (char *)calloc(end_str - ptr_str + 1, sizeof(char));
-    if (result) {
-      s21_strncpy(result, ptr_str, end_str - ptr_str);
-      *(result + (end_str - ptr_str)) = '\0';
-    }
+         end_str--);
+    result = (char*)calloc(end_str - ptr_str + 1, sizeof(char));
+
+    s21_strncpy(result, ptr_str, end_str - ptr_str);
+    *(result + (end_str - ptr_str)) = '\0';
   }
   return result;
 }
