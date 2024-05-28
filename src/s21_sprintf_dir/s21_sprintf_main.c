7e4af8e416b313d0c3a192578ea4ba9d855234db
Kashirskij Nikolaj <nikolajkashirskij@gmail.com>
2024-05-28 20:16:13 +0300

    bla

--- src/s21_sprintf_dir/s21_sprintf_main.c
+++ src/s21_sprintf_dir/s21_sprintf_main.c
@@ -19,7 +19,7 @@ int main() {
   scanf("%29s", string);
 
   // Стандартная sprintf
-  sprintf(f_line, "%+i-% d-%+f%c%s%c", day, month, year, '\t', string, '@');
+  sprintf(f_line, "%+10i-% d-%+f%c%s%c", day, month, year, '\t', string, '@');
   printf("----sprintf-Отформатированная строка: %s\n", f_line);
 
   // Моя sprintf
