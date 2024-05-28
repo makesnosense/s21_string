23240e591fcfb7a5ae1d736a09fce1c19da7b82e
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-28 18:36:21 +0300

    strlen fixes and tests

--- src/s21_strlen.c
+++ src/s21_strlen.c
@@ -1,9 +1,19 @@
 #include "s21_string.h"
+#include "stdio.h"
 
 s21_size_t s21_strlen(const char* str) {
-  const char* ptr = str;
-  while (*ptr != '\0') {
-    ++ptr;
+  s21_size_t res = 0;
+  int err_flag = 0;
+  if (str == S21_NULL) {
+    err_flag = 1;
+    fprintf(stderr,
+            "error: null argument where non-null required (argument 1)\n");
+  } else {
+    const char* ptr = str;
+    while (*ptr != '\0') {
+      ++ptr;
+    }
+    res = ptr - str;
   }
-  return ptr - str;
+  return err_flag == 0 ? res : (s21_size_t)-1;
 }
