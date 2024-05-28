0c5f225bc6c1f8092e767b4961e9a9dc0d6df0d7
AlexBerezovskiy <makesnosense@users.noreply.github.com>
2024-05-28 19:20:28 +0300

    to_lower to_upper fixes and tests

--- src/s21_to_upper.c
+++ src/s21_to_upper.c
@@ -3,12 +3,13 @@
 #include "s21_string.h"
 
 void* s21_to_upper(const char* str) {
-  s21_size_t len = s21_strlen(str);
-  char* result = (char*)calloc(len + 1, sizeof(char));
-  if (result != S21_NULL) {
+  char* result = S21_NULL;
+  if (str != S21_NULL) {
+    s21_size_t len = s21_strlen(str);
+    result = (char*)calloc(len + 1, sizeof(char));
     for (s21_size_t i = 0; i < len; i++) {
       if (str[i] >= 'a' && str[i] <= 'z') {
-        result[i] = str[i] - 32;
+        result[i] = str[i] - ('a' - 'A');
       } else {
         result[i] = str[i];
       }
