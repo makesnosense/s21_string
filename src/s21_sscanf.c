dd211debae39d70cdd8b51a15046772619c94302
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2024-06-27 18:34:13 +0300

    scanf tests first

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -1,5 +1,98 @@
 #include "s21_sscanf.h"
 
+int s21_sscanf(const char *str, const char *format, ...) {
+  va_list args;  // Список аргументов
+  va_start(args, format);  // Инициализируем список аргументов
+
+  Opts opts = {0};  // Зануляем структуру с опциями
+  int result = 0;  // Количество считанных спецификаторов
+
+  // Проходимся по формат-строке
+  while (*format != '\0') {
+    if (*format == '%') {
+      format++;
+      // Считываем * если есть
+      if (*format == '*') {
+        opts.is_star = 1;
+        format++;
+      }
+      // Проверяем наличие цифр после %
+      while (*format >= '0' && *format <= '9') {
+        opts.width = opts.width * 10 + (*format - '0');
+        format++;
+      }
+      switch (*format) {
+        // case 'c':
+        //   char *c = va_arg(args, char *);
+        //   if (read_char(&str, c, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case 's':
+        //   char *s = va_arg(args, char *);
+        //   if (read_string(&str, s, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case 'd':
+        // case 'i':
+        //   int *d = va_arg(args, int *);
+        //   if (read_int(&str, d, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case 'f':
+        //   float *f = va_arg(args, float *);
+        //   if (read_float(&str, f, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case 'u':
+        //   unsigned int *u = va_arg(args, unsigned int *);
+        //   if (read_unsigned_int(&str, u, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case 'x':
+        // case 'X':
+        //   unsigned int *x = va_arg(args, unsigned int *);
+        //   if (read_hex(&str, x, &opts)) {
+        //     if (!opts.is_star) result++;
+        //     format++;
+        //   }
+        //   break;
+        // case '%':
+        //   format++;
+        //   str++;
+        //   break;
+        // case 'n':
+        //   int *n = va_arg(args, int *);
+        //   // YA HYU ZNAYET POCHEMU NO TAK RABOTAET
+        //   int temp = opts.count;
+        //   *n = temp;
+        //   // *n = opts.count;
+        //   format++;
+        //   break;
+        default:
+          printf("%s", str);
+          break;
+      }
+    } else {
+      opts.count++;
+      format++;
+      str++;
+    }
+  }
+  va_end(args);
+
+  return result;
+}
+
 int read_char(const char **str, char *c, Opts *opts) {
   int result = 0;
 
@@ -118,98 +211,6 @@ int read_hex(const char **str, unsigned int *x, Opts *opts) {
     result = 1;
   }
   if (!opts->is_star) *x = num;
-
-  return result;
-}
-
-int s21_sscanf(const char *str, const char *format, ...) {
-  va_list args;  // Список аргументов
-  va_start(args, format);  // Инициализируем список аргументов
-
-  Opts opts = {0};  // Зануляем структуру с опциями
-  int result = 0;  // Количество считанных спецификаторов
-
-  // Проходимся по формат-строке
-  while (*format != '\0') {
-    if (*format == '%') {
-      format++;
-      // Считываем * если есть
-      if (*format == '*') {
-        opts.is_star = 1;
-        format++;
-      }
-      // Проверяем наличие цифр после %
-      while (*format >= '0' && *format <= '9') {
-        opts.width = opts.width * 10 + (*format - '0');
-        format++;
-      }
-      switch (*format) {
-        case 'c':
-          char *c = va_arg(args, char *);
-          if (read_char(&str, c, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case 's':
-          char *s = va_arg(args, char *);
-          if (read_string(&str, s, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case 'd':
-        case 'i':
-          int *d = va_arg(args, int *);
-          if (read_int(&str, d, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case 'f':
-          float *f = va_arg(args, float *);
-          if (read_float(&str, f, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case 'u':
-          unsigned int *u = va_arg(args, unsigned int *);
-          if (read_unsigned_int(&str, u, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case 'x':
-        case 'X':
-          unsigned int *x = va_arg(args, unsigned int *);
-          if (read_hex(&str, x, &opts)) {
-            if (!opts.is_star) result++;
-            format++;
-          }
-          break;
-        case '%':
-          format++;
-          str++;
-          break;
-        case 'n':
-          int *n = va_arg(args, int *);
-          // YA HYU ZNAYET POCHEMU NO TAK RABOTAET
-          int temp = opts.count;
-          *n = temp;
-          // *n = opts.count;
-          format++;
-          break;
-        default:
-          break;
-      }
-    } else {
-      opts.count++;
-      format++;
-      str++;
-    }
-  }
-  va_end(args);
 
   return result;
 }
