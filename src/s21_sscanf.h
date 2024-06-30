9a6189aaf51efc6fad800a62dbeb81a9d418fd19
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2024-06-30 18:47:03 +0300

    added parse specifiers

--- src/s21_sscanf.h
+++ src/s21_sscanf.h
@@ -9,9 +9,19 @@
 
 #include "s21_string.h"
 
+#define VALID_SSCANF_SPECIFIERS "cdin%"
+typedef enum SscanfSpecifier {
+  NOT_SET,
+  c,
+  d,
+  i,
+  n,
+} SscanfSpecifier;
+
 typedef struct SpecifierOptions {
   int width;     // Ширина
   bool is_star;  // Флаг подавления считывания
+  SscanfSpecifier specifier;
 } SpecOptions;
 
 typedef struct InputString {
@@ -19,33 +29,29 @@ typedef struct InputString {
   s21_size_t curr_ind;
 } InputStr;
 
-// Функция для чтения символа
-// int read_char(InputStr* input, char* c, SpecOptions* opts);
+bool n_specifier_follows(InputStr* fmt_input, bool* n_star_present);
+bool c_specifier_follows(InputStr* fmt_input);
 
-// Функция для чтения строки
-int read_string(const char** str, char* s, SpecOptions* opts);
+bool is_end_of_string(InputStr* string_structure);
+bool is_space(char input_char);
+bool is_space_specifier(InputStr* fmt_input);
 
-// Функция для чтения целого числа
+bool we_continue_consuming(InputStr* source, InputStr* fmt_input,
+                           bool* matching_failure);
+
+void consume_space(InputStr* source);
+void consume_initial_space_and_n(va_list* args, InputStr* source,
+                                 InputStr* fmt_input);
+
+int consume_specifier(va_list* args, InputStr* source, InputStr* fmt_input,
+                      bool* matching_failure);
+void process_n(va_list* args, InputStr* source, bool n_star);
+int read_char(va_list* args, InputStr* source, SpecOptions* spec_opts);
 int read_int(InputStr* input, int* d, SpecOptions* opts);
-
-// Функция для чтения беззнакового целого числа
-int read_unsigned_int(const char** str, unsigned int* u, SpecOptions* opts);
-
-// Функция для чтения вещественного числа
-int read_float(InputStr* input, float* f, SpecOptions* opts);
-
-// Функция для чтения шестнадцатеричного числа
-int read_hex(const char** str, unsigned int* x, SpecOptions* opts);
-
-int parse_pointer(InputStr* input, void** value);
-
-void skip_space(InputStr* input);
 
 void parse_width_sscanf(InputStr* fmt_input, SpecOptions* spec_opts);
 
 bool parse_suppression(InputStr* fmt_input);
-
-bool is_space(char input_char);
 
 int source_validity_check(InputStr* source, InputStr* format_input,
                           bool* matching_failure);
@@ -53,12 +59,29 @@ int source_validity_check(InputStr* source, InputStr* format_input,
 int consume_specifier(va_list* args, InputStr* source, InputStr* fmt_input,
                       bool* matching_failure);
 
-bool we_continue_consuming(InputStr* source, InputStr* fmt_input,
-                           bool* matching_failure);
+void process_n(va_list* args, InputStr* source, bool n_star);
+int read_char(va_list* args, InputStr* source, SpecOptions* spec_opts);
 
-bool n_specifier_follows(InputStr* fmt_input, bool* n_star_present);
+void parse_width_sscanf(InputStr* fmt_input, SpecOptions* spec_opts);
+bool parse_suppression(InputStr* fmt_input);
+bool is_sscanf_specifier(char ch);
+void parse_sscanf_specifier(InputStr* fmt_input, SpecOptions* spec_opts);
 
 // // Функция для считывания значений из буфера по формату
 // int s21_sscanf(const char *str, const char *format, ...);
+
+// // Функция для чтения строки
+// int read_string(const char** str, char* s, SpecOptions* opts);
+
+// // Функция для чтения беззнакового целого числа
+// int read_unsigned_int(const char** str, unsigned int* u, SpecOptions* opts);
+
+// // Функция для чтения вещественного числа
+// int read_float(InputStr* input, float* f, SpecOptions* opts);
+
+// // Функция для чтения шестнадцатеричного числа
+// int read_hex(const char** str, unsigned int* x, SpecOptions* opts);
+
+// int parse_pointer(InputStr* input, void** value);
 
 #endif  // S21_SSCANF_H_
